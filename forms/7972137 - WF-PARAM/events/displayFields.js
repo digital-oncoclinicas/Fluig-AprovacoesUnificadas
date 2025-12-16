a88c822c30c89a44febdf8c355c35e8fbd161577
Oncoclinicas Dev Team <dev@oncoclinicas.com>
2025-12-16 14:32:13 -0300

    AJUSTES PROJETO PA VR-VA

--- forms/7972137 - WF-PARAM/events/displayFields.js
+++
@@ -1,9 +0,0 @@
-/*    SM2CE LTDA
-     ANALISTA RICARDO ANDRADE
-             ATENCAO
-  *** NAO IDENTAR ESTE CODIGO ***
-*/
-function displayFields(form, customHTML) {
-	customHTML.append("<style> form {margin: 0px 50px 50px 0px;} </style> ");
-    form.setHidePrintLink(true);
-}
